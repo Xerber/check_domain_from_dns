5ad56bc21f855d97ca7371f046f157323ee6a7d0
Xerber <cherobuk.91@gmail.com>
2020-05-16 11:34:54 +0300

    added ability to work with cyrillic domains

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ with open(name_file, encoding='utf-8') as file:
 
 for dns in dnslist:
   for domain in f1:
+    domain = domain.encode('idna')
+    domain = domain.decode('utf-8')
     dns_query = Nslookup(dns_servers=[dns])
     ips_record = dns_query.dns_lookup(domain)
     if '127.0.0.1' not in ips_record.answer:
+      domain = domain.encode('idna')
+      domain = domain.decode('idna')
       logging.warning(f'{domain}:{dns}')
     else: pass
